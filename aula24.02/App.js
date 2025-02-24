4d6dbbc5dccc04df4bbbf4e685157654ecbbc37a
JoaoMichaeli <161324070+JoaoMichaeli@users.noreply.github.com>
2025-02-24 09:22:51 -0300

    css finalizado

--- aula24.02/App.js
+++ aula24.02/App.js
@@ -4,8 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{
+        alignSelf: 'stretch', // ocupe todo o espaço
+        flex: 5, // seja 50% da tela
+        backgroundColor: '#f00', // mudando a cor
+        alignItems: 'center', // alinhando os itens
+        justifyContent: 'center' // centralizando os itens
+    }}> 
+        <Text>Bom dia!</Text>
+        </View>
+      <View style={{
+        flex: 5,
+        alignSelf: 'stretch',
+        backgroundColor: 'skyblue',
+        alignItems: 'center',
+        justifyContent: 'center'}}>
+        <Text>Turma Fiap!</Text>
+        </View>
     </View>
   );
 }
